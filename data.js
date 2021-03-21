af96299ed2dedca779427073d192e79142d55263
nihao-greece <80563350+nihao-greece@users.noreply.github.com>
2021-03-21 21:59:43 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,67 @@
+var data = [
+    {
+        "category": "Food",
+        "products": [
+            {
+                "product-image": "assets/img/products/product-3.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$90.00",
+                "product-old-price": "$100.00"
+            },
+            {
+                "product-image": "assets/img/products/product-5.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$60.00",
+                "product-old-price": "$70.00"
+            },
+            {
+                "product-image": "assets/img/products/product-6.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$190.00"
+            },
+            {
+                "product-image": "assets/img/products/product-7.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$150.00",
+                "product-old-price": "$170.00"
+            },
+            {
+                "product-image": "assets/img/products/product-8.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$80.00"
+            },
+            {
+                "product-image": "assets/img/products/product-9.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$180.00",
+                "product-old-price": "$190.00"
+            },
+            {
+                "product-image": "assets/img/products/product-10.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$70.00"
+            }
+        ]
+    },
+    {
+        "category": "Drink",
+        "products": [
+            {
+                "product-image": "assets/img/products/product-1.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$100.00",
+                "product-old-price": "$120.00"
+            },
+            {
+                "product-image": "assets/img/products/product-2.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$200.00"
+            },
+            {
+                "product-image": "assets/img/products/product-4.jpg",
+                "product-name": "PRODUCTS NAME HERE",
+                "product-price": "$50.00"
+            }
+        ]
+    }
+]
